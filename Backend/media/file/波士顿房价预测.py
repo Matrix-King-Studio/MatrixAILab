ebbdf71586bd1e95e23268e1b789217f22a4e33b
Alex 007 <alex18812649207@gmail.com>
2020-12-28 20:49:48 +0800

    添加数据库

--- Backend/media/file/波士顿房价预测.py
+++ Backend/media/file/波士顿房价预测.py
@@ -1,3 +1,5 @@
+import pickle
+
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -12,16 +14,53 @@ def readCsv(path, header):
 
 
 if __name__ == '__main__':
-    trainData = readCsv("train.csv", 0)
+    # 读取训练数据集
+    trainData = readCsv(r"G:\Project\MatrixAILab\Backend\media\file\train.csv", 0)
 
-    X_train, X_test, y_train, y_test = trainTestSplit(trainData[:, :-1], trainData[:, -1], 0.33, 42)
+    trainData.drop("Id", axis=1, inplace=True)
 
+    # 数据预处理
+    na_count = trainData.isnull().sum().sort_values(ascending=False)
+    na_rate = na_count / len(trainData)
+    na_data = pd.concat([na_count, na_rate], axis=1, keys=['count', 'ratio'])
+    trainData.drop(na_data[na_data['count'] > 1].index, axis=1, inplace=True)
+    trainData.drop(trainData.loc[trainData['Electrical'].isnull()].index, inplace=True)
+    for col in trainData.columns:
+        if trainData[col].dtypes == "object":
+            trainData[col], uniques = pd.factorize(trainData[col])
+
+    # 训练/测试集划分
+    XData, yData = trainData.iloc[:, :-1], trainData.iloc[:, -1]
+    X_train, X_test, y_train, y_test = trainTestSplit(XData, yData, 0.33, 42)
+
+    # 线性回归模型
     lr = LinearRegression()
 
+    # 拟合
     lr.fit(X_train, y_train)
 
+    # 评测
     score = lr.score(X_test, y_test)
+    print(score)
 
-    testData = readCsv("test.csv", 0)
+    # 读取测试数据集
+    testData = readCsv(r"G:\Project\MatrixAILab\Backend\media\file\test.csv", 0)
 
+    # 数据预处理
+    testID = testData["Id"]
+    testData.drop("Id", axis=1, inplace=True)
+    testData.drop(na_data[na_data['count'] > 1].index, axis=1, inplace=True)
+    for col in testData.columns:
+        if testData[col].dtypes == "object":
+            testData[col], uniques = pd.factorize(testData[col])
+        testData[col].fillna(testData[col].mean(), inplace=True)
+
+    # 预测
     predict = lr.predict(testData)
+
+    # 保存模型
+    with open(r"G:\Project\MatrixAILab\Backend\media\file\model.pkl", "wb") as fp:
+        pickle.dump(lr, fp)
+
+    submit = pd.concat([testID, pd.Series(abs(predict))], axis=1, keys=["Id", "SalePrice"])
+    submit.to_csv(r"G:\Project\MatrixAILab\Backend\media\file\submisson.csv", index=False)

28e5589736310640ff5c90cbb6aea7b1c0c3eb37
hero-heng <2374114527@qq.com>
2021-01-08 09:42:14 +0800

    加入注册

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Login from '@/views/Login.vue';
 import editor from "@/views/editor";
 import register from "@/views/register";
 import forgetpass from "@/views/forgetpass"
+import { from } from 'core-js/fn/array';
 
 Vue.use(VueRouter)
 
@@ -39,9 +40,21 @@ const routes = [{
 ]
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
-})
+        mode: 'history',
+        base: process.env.BASE_URL,
+        routes
+    })
+    // 挂载路由导航守卫
+    // router.beforeEach((to, from, next) => {
+    //     // to将要访问的路径
+    //     // from 代表从哪个路径跳转而来
+    //     // next是一个函数，表示放行
+    //     //next()放行     next('/login')强制跳转
+    //     if (to.path === '/login') return next();
+    //     //获取token
+    //     const tokenStr = window.sessionStorage.getItem('token')
+    //     if (!tokenStr) return next('/login')
+    //     next()
+    // })
 
 export default router
